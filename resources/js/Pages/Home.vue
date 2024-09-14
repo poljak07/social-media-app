263bf6d291171f32f3552a16a0338103f8aff3ca
poljak07 <acmeta4@gmail.com>
2024-09-14 23:08:42 +0200

    Use AuthenticatedLayout on home to display navbar

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,26 +4,29 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowingList from "@/Components/app/FollowingList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 </script>
 
 <template>
     <Head title="Social Media Website"/>
 
-    <div className="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div className="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList/>
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList/>
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList/>
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <CreatePost/>
+                <PostList class="flex-1"/>
+            </div>
         </div>
-        <div className="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList/>
-        </div>
-        <div className="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <CreatePost/>
-            <PostList class="flex-1"/>
-        </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
 
 <style>
-/* Tvoj stil ovde */
+
 </style>
