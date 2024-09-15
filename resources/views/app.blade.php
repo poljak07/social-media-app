e8caa95310ebe221806f626493da01104ec3107c
poljak07 <acmeta4@gmail.com>
2024-09-15 21:13:11 +0200

    Implement user profile page UI

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="lg:h-full">
 <head>
-    <head>
-        <meta charset="utf-8">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @@ -15,7 +15,7 @@
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-        @inertiaHead
-    </head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
 </head>
-<body class="font-sans antialiased">
 <body class="font-sans antialiased bg-gray-100 lg:overflow-hidden lg:h-full">
 @inertia
-@inertia
 </body>
-</body>
-</html>
 </html>
