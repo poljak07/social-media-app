7fa8b7579498f540f8f0b18a21c30df3238871f2
poljak07 <acmeta4@gmail.com>
2024-10-15 15:55:09 +0200

    Query all comments of posts with a single select and construct tree in PHP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,24 +13,16 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $userId = Auth::id();
-        $posts = Post::query()
-            ->withCount('reactions')
-            ->withCount('comments')
-            ->with([
-                'comments' => function ($query) use ($userId) {
-                    $query
-                        ->whereNull('parent_id')
-                        ->withCount('reactions')
-                        ->withCount('comments')
-                        ->with([
-                            'reactions' => function ($query) use ($userId) {
-                                $query->where('user_id', $userId);
-                            }
-                        ]);
-                },
-                'reactions' => function ($query) use ($userId) {
-                    $query->where('user_id', $userId);
-                }])
+        $posts = Post::query() // SELECT * FROM posts
+        ->withCount('reactions') // SELECT COUNT(*) from reactions
+        ->with([
+            'comments' => function ($query) use ($userId) {
+                $query->withCount('reactions'); // SELECT * FROM comments WHERE post_id IN (1, 2, 3...)
+                // SELECT COUNT(*) from reactions
+            },
+            'reactions' => function ($query) use ($userId) {
+                $query->where('user_id', $userId); // SELECT * from reactions WHERE user_id = ?
+            }])
             ->latest()
             ->paginate(20);
 
