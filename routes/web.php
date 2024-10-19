6d0631614a92a9dd81537aae920cc2b2009e152d
poljak07 <acmeta4@gmail.com>
2024-10-19 22:14:20 +0200

    Implement group profile page with cover and thumbnail upload

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
 Route::get('/', [HomeController::class, 'index'])
     ->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -15,9 +16,16 @@ Route::get('/', [HomeController::class, 'index'])
 Route::get('/u/{user:username}', [ProfileController::class, 'index'])
     ->name('profile');
 
+Route::get('/g/{group:slug}', [GroupController::class, 'profile'])
+    ->name('group.profile');
+
 Route::middleware('auth')->group(function () {
     Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
         ->name('profile.updateImages');
+
+    Route::post('/group/update-images/{group:slug}', [GroupController::class, 'updateImage'])
+        ->name('group.updateImages');
+
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
